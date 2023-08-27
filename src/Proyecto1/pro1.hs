8dc8aeff1c67126f8d18760816a25fc264f23d85
VillarPedr0 <pedrovillar171@gmail.com>
2023-08-27 17:00:57 -0300

    Update pro1.hs

--- src/Proyecto1/pro1.hs
+++ src/Proyecto1/pro1.hs
@@ -1,3 +1,4 @@
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
 -- 1. Programá las siguientes funciones:
 --    a) esCero :: Int -> Bool, que verifica si un entero es igual a 0.
 esCero :: Int -> Bool
@@ -17,7 +18,7 @@ True
 ghci> esPositivo (-4)
 False
 -}
---    c ) esVocal :: Char -> Bool, que verifica si un caracter es una vocal en minuscula.
+--    c ) esVocal :: Char -> Bool, que verifica si un caracter es una vocal en minúscula.
 esVocal :: Char -> Bool
 esVocal x = x `elem` "aeiou"
 {-
@@ -26,8 +27,11 @@ True
 ghci> esVocal 'v'
 False
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
--- 2. Programa las siguientes funciones usando recursion o composicion:
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 2. Programá las siguientes funciones usando recursión o composición:
 --    a) paratodo :: [Bool] -> Bool, que verifica que todos los elementos de una lista
 --    sean True.
 paraTodo :: [Bool] -> Bool
@@ -39,11 +43,15 @@ False
 ghci> paraTodo [True, True]
 True
 -}
--- ____________________________________________________________________________________________ --
--- Se puede utilizar la función and que retorna true si todos los elementos de la lista son true
--- paraTodo :: [Bool] -> Bool
--- paraTodo = and
--- ____________________________________________________________________________________________ --
+{-
+    Se podría utilizar la función and que retorna true si todos los elementos de la lista son true
+    .----------------------------------------. 
+    :                                        : 
+    :   paraTodo :: [Bool] -> Bool           : 
+    :   paraTodo = and                       :        
+    :                                        : 
+    `----------------------------------------'  
+-}
 --    b) sumatoria :: [Int] -> Int, que calcula la suma de todos los elementos de una
 --    lista de enteros.
 sumatoria :: [Int] -> Int
@@ -66,7 +74,7 @@ ghci> productoria [1,2,3]
 ghci> productoria [1,2,3,30,21]
 3780
 -}
---    d ) factorial :: Int -> Int, que toma un ńumero n y calcula n!.
+--    d ) factorial :: Int -> Int, que toma un número n y calcula n!.
 factorial :: Int -> Int
 factorial x = product [1..x]
 {-
@@ -75,8 +83,8 @@ ghci> factorial 4
 ghci> factorial 19
 121645100408832000
 -}
---    e) Utiliza la funcion sumatoria para definir, promedio :: [Int] -> Int, que toma
---    una lista de numeros no vacia y calcula el valor promedio (truncado, usando division
+--    e) Utiliza la función sumatoria para definir, promedio :: [Int] -> Int, que toma
+--    una lista de números no vacia y calcula el valor promedio (truncado, usando division
 --    entera).
 promedio :: [Int] -> Int
 promedio [] = 0
@@ -87,8 +95,11 @@ ghci> promedio [1,2,3]
 ghci> promedio [9,2,8,1,3]
 3
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
--- 3. Programa la funcion pertenece :: Int -> [Int] -> Bool, que verifica si un numero se
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 3. Programá la función pertenece :: Int -> [Int] -> Bool, que verifica si un número se
 -- encuentra en una lista.
 pertenece :: Int -> [Int] -> Bool
 pertenece _ [] = False
@@ -99,9 +110,12 @@ True
 ghci> pertenece 3 [1,2,1,6,8,2,4]
 False
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
--- 4. Programa las siguientes funciones que implementan los cuantificadores generales. Nota que
--- el segundo parametro de cada funcion, es otra funcion!
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 4. Programá las siguientes funciones que implementan los cuantificadores generales. Notá que
+-- el segundo parámetro de cada función, es otra función!
 --    a) paratodo’ :: [a] -> (a -> Bool) -> Bool, dada una lista xs de tipo [a] y un
 --    predicado t :: a -> Bool, determina si todos los elementos de xs satisfacen el
 --    predicado t.
@@ -115,7 +129,7 @@ ghci> paraTodo' [0,0,0,0] esCero
 True
 -}
 --    b) existe’ :: [a] -> (a -> Bool) -> Bool, dada una lista xs de tipo [a] y un
---    predicado t :: a -> Bool, determina si alǵun elemento de xs satisface el predicado
+--    predicado t :: a -> Bool, determina si algún elemento de xs satisface el predicado
 --    t.
 existe' :: [a] -> (a -> Bool) -> Bool
 existe' [] f = False
@@ -127,8 +141,8 @@ ghci> existe' [8,9,23,4] esCero
 False
 -}
 --    c ) sumatoria’ :: [a] -> (a -> Int) -> Int, dada una lista xs de tipo [a] y una
---    funcion t :: a -> Int (toma elementos de tipo a y devuelve enteros), calcula la
---    suma de los valores que resultan de la aplicacion de t a los elementos de xs.
+--    función t :: a -> Int (toma elementos de tipo a y devuelve enteros), calcula la
+--    suma de los valores que resultan de la aplicación de t a los elementos de xs.
 sumatoria' :: [a] -> (a -> Int) -> Int
 sumatoria' [] f = 0
 sumatoria' (x:xs) f = f x + sumatoria' xs f
@@ -137,8 +151,8 @@ ghci> sumatoria' [1,2,3,4] factorial
 33
 -}
 --    d ) productoria’ :: [a] -> (a -> Int) -> Int, dada una lista de xs de tipo [a]
---    y una funcion t :: a -> Int, calcula el producto de los valores que resultan de la
---    aplicacion de t a los elementos de xs.
+--    y una función t :: a -> Int, calcula el producto de los valores que resultan de la
+--    aplicación de t a los elementos de xs.
 productoria' :: [a] -> (a -> Int) -> Int
 productoria' [] f = 1
 productoria' (x:xs) f = f x * productoria' xs f
@@ -146,10 +160,12 @@ productoria' (x:xs) f = f x * productoria' xs f
 ghci> productoria' [1,2,3,4] factorial
 288
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
 
--- 5. Definí nuevamente la funcion paratodo, pero esta vez usando la funcion paratodo’ (sin
--- recursion ni analisis por casos!). 
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 5. Definí nuevamente la función paratodo, pero esta vez usando la función paratodo’ (sin
+-- recursión ni analisis por casos!). 
 esTrue x = x == True
 paratodo'' :: [Bool] -> Bool
 paratodo'' xs = paraTodo' xs esTrue
@@ -159,10 +175,13 @@ False
 ghci> paratodo'' [True,True,True]
 True
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
 -- 6. Utilizando las funciones del ejercicio 4, programa las siguientes funciones por composicíon,
 -- sin usar recursion ni analisis por casos.
---    a) todosPares :: [Int] -> Bool verifica que todos los numeros de una lista sean
+--    a) todosPares :: [Int] -> Bool verifica que todos los números de una lista sean
 --    pares.
 todosPares :: [Int] -> Bool
 todosPares xs = paraTodo' xs even
@@ -174,8 +193,8 @@ True
 ghci> todosPares [6,2,4]
 True
 -}
---    b) hayMultiplo :: Int -> [Int] -> Bool verifica si existe algun numero dentro del
---    segundo parametro que sea multiplo del primer parametro.
+--    b) hayMultiplo :: Int -> [Int] -> Bool verifica si existe algun número dentro del
+--    segundo parámetro que sea múltiplo del primer parámetro.
 esMultiplo :: Int -> Int -> Bool
 esMultiplo a b = mod b a == 0
 hayMultiplo :: Int -> [Int] -> Bool
@@ -198,8 +217,8 @@ ghci> sumaCuadrados 3
 ghci> sumaCuadrados 8
 140
 -}
---   d ) Programar la fucion existeDivisor::Int-> [Int] -> Bool, que dado en entero n
---   y una lista ls , devuelve True si y solo si, existe algun elemento en ls que divida a na
+--   d ) Programar la función existeDivisor::Int-> [Int] -> Bool, que dado en entero n
+--   y una lista ls , devuelve True si y solo si, existe algún elemento en ls que divida a na.
 esDivisor :: Int -> Int -> Bool
 esDivisor n d = n `mod` d == 0
 existeDivisor :: Int -> [Int] -> Bool
@@ -220,7 +239,7 @@ True
 ghci> esPrimo 4
 False
 -}
--- f) ¿Se te ocurre como redefinir factorial (ej. 2d ) para evitar usar recursi ́on?
+-- f) ¿Se te ocurre como redefinir factorial (ej. 2d ) para evitar usar recursión?
 {-factorial :: Int -> Int
 factorial x = product [1..x]-}
 -- g ) Programar la función multiplicaPrimos :: [Int] -> Int que calcula el producto
@@ -228,22 +247,22 @@ factorial x = product [1..x]-}
 multiplicaPrimos :: [Int] -> Int
 multiplicaPrimos xs = product (filter esPrimo xs)
 -- h) Programar la función esFib :: Int -> Bool, que dado un entero n, devuelve True
--- si y s ́olo si n está en la sucesión de Fibonacci.
+-- si y sólo si n está en la sucesión de Fibonacci.
 fib :: Int -> Int
 fib n | n <= 0    = 0
       | n == 1    = 1
       | otherwise = fib (n - 1) + fib (n - 2)
-esFib :: Int -> Bool
-esFib n = n `elem` [fib i | i <- [0..(n+1)]]
+esFib' :: Int -> Bool
+esFib' n = n `elem` [fib i | i <- [0..(n+1)]]
 {-
 ghci> esFib 5
 True
 ghci> esFib 10
 False
 -}
--- i ) Utilizando la funci ́on del apartado anterior, defin ́ı la funci ́on todosFib :: [Int] -> Bool
+-- i ) Utilizando la función del apartado anterior, definí la función todosFib :: [Int] -> Bool
 -- que dada una lista xs de enteros, devuelva si todos los elementos de la lista pertenecen
--- (o no) a la sucesi ́on de Fibonacci.
+-- (o no) a la sucesión de Fibonacci.
 todosFib :: [Int] -> Bool
 todosFib xs = paraTodo' xs esFib
 {-
@@ -252,7 +271,11 @@ False
 ghci> todosFib [3,5,8,89]
 True
 -}
--- 7. Indag ́a en Hoogle sobre las funciones map y filter. Tambi ́en podes consultar su tipo en
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
+
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 7. Indagá en Hoogle sobre las funciones map y filter. También podes consultar su tipo en
 -- ghci con el comando :t.
 --                               ¿Qué hacen estas funciones?
 {-
@@ -276,15 +299,18 @@ en True, x es incluido a la lista.
     :                   | otherwise = filter p xs   : 
     :                                               : 
     `------------------------------------------- ---'    
--}
---            ¿A qué equivale la expresión map succ [1, -4, 6, 2, -8], donde succ n = n+1?
-{-
+
+              ¿A qué equivale la expresión map succ [1, -4, 6, 2, -8], donde succ n = n+1?
+
 Equivale a la lista [2,-3,7,3,-7], donde cada elemento es el siguiente de la lista dada.
--}
---                       ¿Y la expresi ́on filter esPositivo [1, -4, 6, 2, -8]?
+
+                       ¿Y la expresión filter esPositivo [1, -4, 6, 2, -8]?
 --A la lista de los positivos [1,6,2] pertenecientes a la lista dada.
+-}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
 
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
 -- 8. Programá una función que dada una lista de números xs, devuelve la lista que resulta de
 -- duplicar cada valor de xs.
 --    a) Definila usando recursión.
@@ -302,7 +328,10 @@ dupLista' xs = map (*2) xs
 ghci> dupLista' [1,2,3,4]
 [2,4,6,8]
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
+
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
 -- 9. Programá una función que dada una lista de números xs, calcula una lista que tiene como
 -- elementos aquellos números de xs que son primos.
 --    a) Definila usando recursión.
@@ -330,8 +359,10 @@ ghci> primListas' [1..100]
 multiplicaPrimos :: [Int] -> Int
 multiplicaPrimos xs = product (filter esPrimo xs)
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
 
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
 -- 10. La función primIgualesA toma un valor y una lista, y calcula el tramo inicial más largo de
 -- la lista cuyos elementos son iguales a ese valor.
 --    a) Programá primIgualesA por recursión.
@@ -354,9 +385,11 @@ ghci> primIgualesA' 1 [1,1,2,3,4,1,1,1]
 ghci> primIgualesA' 2 [1,2,2,2,2,2,2,2]
 []
 -}
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
 
 
--- 11. La funci ́on primIguales toma una lista y devuelve el mayor tramo inicial de la lista cuyos
+-- ╔═══════════════════════════════════════════════════════════════════════════════════════════╗ --
+-- 11. La función primIguales toma una lista y devuelve el mayor tramo inicial de la lista cuyos
 -- elementos son todos iguales entre sí.
 --    a) Programá primIguales por recursión
 primIguales :: (Eq a) => [a] -> [a] 
@@ -379,6 +412,4 @@ ghci> primIguales' [4,4,3,4,4,4,4,4,1]
 ghci> primIguales' [3,4,3,4,4,4,4,4,1]
 [3]
 -}
-
-
--- 
+-- ╚═══════════════════════════════════════════════════════════════════════════════════════════╝ --
